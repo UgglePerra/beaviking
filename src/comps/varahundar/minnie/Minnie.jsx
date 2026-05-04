11fdad94816539cc1f18a9a0eeee21c6602ec1b6
UgglePerra <per.timborn@gmail.com>
2026-05-04 09:04:43 +0200

    4maj0904

--- src/comps/varahundar/minnie/Minnie.jsx
+++ src/comps/varahundar/minnie/Minnie.jsx
@@ -10,8 +10,7 @@ import img8 from "./imgs/img8.jpeg";
 import img9 from "./imgs/img9.jpeg";
 import img10 from "./imgs/img10.jpeg";
 
-
-export default function Minnie () {
+export default function Minnie() {
   return (
     <div className="gallery-page">
       <div className="text">
@@ -22,46 +21,97 @@ export default function Minnie () {
           Född 8 april 2024
           <br />
           <br />
-          
-          Minnie är vårt lilla underverk, en gåva från ovan: jo hon kommer från Luleå (!) Anna och Linnea, Kennel Evan’s Aha. Hennes mormor Vickan, Be A Viking Berylla Boffin, är född här hemma hos oss och hennes mormorsmor Malva, Peach Blossom Blue Temptation var vår första tik som jag hämtade hem från östra Tyskland. Minnie har fyra av våra hundar bakom sig i stamtavlan.
+          Minnie är vårt lilla underverk, en gåva från ovan: jo hon kommer från
+          Luleå (!) Anna och Linnea, Kennel Evan’s Aha. Hennes mormor Vickan, Be
+          A Viking Berylla Boffin, är född här hemma hos oss och hennes
+          mormorsmor Malva, Peach Blossom Blue Temptation var vår första tik som
+          jag hämtade hem från östra Tyskland. Minnie har fyra av våra hundar
+          bakom sig i stamtavlan.
           <br />
           <br />
-          Men främst är hon en rolig, kavat och otroligt vacker ung dam. Så kärleksfull; överöser alla med nospussar, nyfiken och smart! Minnie har utnämnt sig själv till chefen här hemma!! Hon har också gjort det bra i utställningsringarna; Köpenhavns Vinnare 2025, Köpenhavns Junior Vinnare 2025, Bäst i Rasen Askersund, ett flertal Bästa tik placeringar och championcertifikat i både Sverige och Danmark.
+          Men främst är hon en rolig, kavat och otroligt vacker ung dam. Så
+          kärleksfull; överöser alla med nospussar, nyfiken och smart! Minnie
+          har utnämnt sig själv till chefen här hemma!! Hon har också gjort det
+          bra i utställningsringarna; Köpenhavns Vinnare 2025, Köpenhavns Junior
+          Vinnare 2025, Bäst i Rasen Askersund, ett flertal Bästa tik
+          placeringar och championcertifikat i både Sverige och Danmark.
           <br />
           <br />
-         
         </div>
       </div>
       <img src={img1} alt="main" className="dog-image" />
-      <br /><br /><br /><br /><br /><br />
-      
-      <img src={img2} alt="main" className="dog-image" />
-      <br /><br /><br /><br /><br /><br />
-      
-      <img src={img3} alt="main" className="dog-image" />
-      <br /><br /><br /><br /><br /><br />
-      
-      <img src={img4} alt="main" className="dog-image" />
-      <br /><br /><br /><br /><br /><br />
-      
-      <img src={img5} alt="main" className="dog-image" />
-      <br /><br /><br /><br /><br /><br />
-      
-      <img src={img6} alt="main" className="dog-image" />
-      <br /><br /><br /><br /><br /><br />
-      
-      <img src={img7} alt="main" className="dog-image" />
-      <br /><br /><br /><br /><br /><br />
-      
-      <img src={img8} alt="main" className="dog-image" />
-      <br /><br /><br /><br /><br /><br />
-      
-      <img src={img9} alt="main" className="dog-image" />
-      <br /><br /><br /><br /><br /><br />
-      
-      <img src={img10} alt="main" className="dog-image" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
-      
+      <img src={img2} alt="main" className="dog-image" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <img src={img3} alt="main" className="dog-image" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <img src={img4} alt="main" className="dog-image" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <img src={img5} alt="main" className="dog-image" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <img src={img6} alt="main" className="dog-image" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <img src={img7} alt="main" className="dog-image" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <img src={img8} alt="main" className="dog-image" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <img src={img9} alt="main" className="dog-image" />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <img src={img10} alt="main" className="dog-image" />
     </div>
   );
 }

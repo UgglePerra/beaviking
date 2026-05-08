768242f716cb5dc8cc6b72e538db1363b2352af9
UgglePerra <per.timborn@gmail.com>
2026-05-08 13:07:40 +0200

    8maj1307

--- src/comps/Resultat.jsx
+++ src/comps/Resultat.jsx
@@ -12,7 +12,215 @@ export default function Resultat() {
     <div className="gallery-page">
       <div className="text">
         <h1>Resultat/Champions</h1>
+        Vi har haft glädjen att våra hundar och deras avkommor har varit framgångsrika i utställningsringarna. Nedan en sammanställning på meriterade hundar utan inbördes ordning;
       </div>
+
+      <div className="text-spacer">
+        <h3>
+          EUW-15 JWW-14 FI UCH NO JV-14 NO UCH SE UCH Be A Viking Bell Gamgee
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          C.I.B. NO V-15 SE JV-13 NO V-13 NO JV-13 NORD UCH Be A Viking Anduril
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          SE UCH DK UCH KBHV-25 KBHJV-25 Evan’s Aha Magic Be A Viking
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          SE UCH FI UCH Tilevis Flitwick Be A Viking
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          C.I.B. SE V-12 NORD V-10 NO V-12 NO V-08 NORD&FI UCH Buenazo&apos;s Viking
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          C.I.B. DK UCH NO UCH SE UCH Peach Blossom Blue Temptation
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          NORD JV-14 NO JV-14 Be A Viking Bilbo Baggins
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          SE JV-13 DK JV-13 SE UCH DK UCH Skystar&apos;s Louis
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          C.I.B. DK UCH NO UCH SE UCH Honungsmåne Love Me Do
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          SE V-15 FI JV-14 SE UCH FI UCH NO UCH Be A Viking Berylla Boffin
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          NORD JV-14 NO JV-14 Be A Viking Bilbo Baggins
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          Ck, Be A Viking Belladonna Took
+          Excellent, Be A Viking Doris Day
+        </h3>
+      </div>
+
+      <br />
+
+      <div className="text-spacer">
+        <h3>
+          Meriterade avkommor till våra hundar under annat prefix:
+        </h3>
+      </div>
+
+       <div className="text-spacer">
+        <h3>
+          C.I.B. SE V-12 NORD V-10 NO V-12 NO V-08 NORD&FI UCH Buenazo&apos;s Viking:
+        </h3>
+      </div>
+
+       <div className="text-spacer">
+        <h3>
+          C.I.B. DK UCH NO UCH SE UCH Honungsmåne Love Me Do 
+        </h3>
+      </div>
+
+       <div className="text-spacer">
+        <h3>
+          SE UCH Honungsmåne Love to Love
+        </h3>
+      </div>
+
+      <div className="text-spacer">
+        <h3>
+          SE UCH Honungsmåne Lucky Star
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          NO UCH Honungsmåne Lover Boy
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          SE JV-13 DK JV-13 SE UCH DK UCH Skystar's Louis:
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          DKCH KBHV10 BigChoice RosalinaMiaMaya
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          SE UCH Xan-Liz v Enzzo
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          NO UCH AU CH Valpgläntans Viktor
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          DK UCH Rubini Catch The Dream Chowder
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          C.I.B. DK UCH NO UCH SE UCH Honungsmåne Love Me Do:
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          C.I.B. DK V-11 NORD UCH Kläppas Anna Sui Love
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          NORD UCH Kläppas A Curious Love
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          DK UCH SE UCH Rubini Isabella Love Me Do
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          IT CH Bichavas Bianca
+        </h3>
+      </div>
+      
+      <div className="text-spacer">
+        <h3>
+          SE JV-13 DK JV-13 SE UCH DK UCH Skystar's Louis:
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          SE VV-24 DK UCH SE UCH NO UCH Meandi’s Amazing Alba
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          Multi-Champion VD CH Elrond von Heppenheimer
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          CERT R-CACIB Ck Voila Give Me Five
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          CERT R-CACIB Ck Sweet Bishamons Dance Of Dragons
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          RLD F RLD N Sweet Bishamons King In the North
+        </h3>
+      </div>
+      <div className="text-spacer">
+        <h3>
+          FI UCH SE UCH Tilevis Flitwick Be A Viking
+        </h3>
+      </div>
+      
+      
+
+
+
+
+
+
+
+
+
 
       <div className="img-spacer">
         <img src={img1} alt="main" className="dog-image" />

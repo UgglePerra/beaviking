ae62524cc7c4f1af43a0ac72b0ec716cdc0bdb4d
UgglePerra <per.timborn@gmail.com>
2026-06-16 22:59:07 +0200

    removedImageMakerandSmallImageMaker

--- src/comps/imagemaker/ImageMaker.jsx
+++
@@ -1,8 +0,0 @@
-function img_maker (img) {
-    return(
-      <div className="img-spacer">
-        <img src={img} alt="main" className="dog-image" />
-      </div>)
-  }
-
-  export default ImageMaker
--- src/comps/imagemaker/SmallImageMaker.jsx
+++
@@ -1,8 +0,0 @@
-function small_img_maker (img) {
-    return(
-      <div className="small-img-spacer">
-        <img src={img} alt="main" className="dog-image" />
-      </div>)
-  }
-
-export default SmallImageMaker

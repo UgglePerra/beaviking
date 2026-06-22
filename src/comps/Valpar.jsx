08457ad1196818611fde2a3bb95829c0093c4d34
UgglePerra <per.timborn@gmail.com>
2026-06-22 13:59:49 +0200

    22jun1359

--- src/comps/Valpar.jsx
+++ src/comps/Valpar.jsx
@@ -28,10 +28,34 @@ export default function Valpar() {
     <div className="gallery-page">
       <div className="text-over-img">
         <h1>Valpar</h1>
-        Är du intresserad av en valp efter våra hundar så är du välkommen att höra av dig! Vi hälsotestar våra hundar regelbundet och följer SKK&apos;s grundregler och avelspolicy samt BBHC&apos;s avelspolicy. <br /><br />Våra hundar som används i avel är hälsotestade utan anmärkning avseende hjärta, ögon och knän 0/0 enligt rasklubbens riktlinjer. De är utställda med fina resultat vilket visar på utmärkt mentalitet och exteriör.<br /><br />
-        Anna-Karin och Ida Jernberg<br />
+        Är du intresserad av en valp efter våra hundar så är du välkommen att höra av dig! Vi hälsotestar våra hundar regelbundet och följer SKK's grundregler, avelspolicy samt BBHC's avelsrekommendationer. BBHC har även förslag på frågor att ställa till uppfödare inför planerna på att köpa en valp. Nedan hittar du en länk till dessa.
+        <br /><br />
+        Vi är naturligtvis angelägna om att lära känna dig, din familj och den omgivande miljön som valpen kommer flytta till. Vi finns tillgängliga för våra valpköpare för frågor, tips och stöd, inte bara under valptiden utan även fortsatt. För att undvika små problem och onödiga veterinärbesök så delar vi gärna med oss av bra tips och erfarenheter. 
+        <br /><br />
+        Vi vill att ni ska få ett fantastiskt liv tillsammans!
+        <br /><br />
+        Våra hundar som används i avel är hälsotestade utan anmärkning avseende hjärta, ögon och knän 0/0 enligt rasklubbens riktlinjer. De är utställda med fina resultat vilket visar på utmärkt mentalitet och exteriör.
+        <br /><br />
 
-        Anna-Karin.Jernberg@telia.com
+
+        Anna-Karin och Ida Jernberg
+
+        <br /><br />
+
+        Mail:&nbsp; 
+        <span>
+          <a href="mailto:anna-karin.jernberg@telia.com">
+          anna-karin.jernberg@telia.com
+          </a>
+        </span>
+
+        <br /><br />
+    
+        <a href="https://www.bbhc.se/kopa-hund/">
+          https://www.bbhc.se/kopa-hund/
+        </a>
+           
+      
        
       </div>
 

4b76fe8265f6c0db19bed495a265fd6548343231
UgglePerra <per.timborn@gmail.com>
2026-07-04 08:43:20 +0200

    4jul0843

--- src/comps/varahundar/vicke/Vicke.jsx
+++ src/comps/varahundar/vicke/Vicke.jsx
@@ -81,6 +81,17 @@ export default function Vicke() {
         </div>
 
         {img_maker(img4)}
+
+        <div className="text-over-img">
+          <h3>Nordic Dog Show Borås 2026
+          <br />
+          Bästa Hane 2 Reserv Nordic Show Cert</h3>
+          Bedömning: 2 år, utmärkt storlek. Fina proportioner avseende höjd och längd.
+          Välformad skalle, med bra längd i nospartiet. Mörka uttrycksfulla ögon.
+          Tillräcklig hals, utmärkt överlinje och kors. Väl utfylld bröstkorg med mycket bra kropp. Passande benstomme. Korrekta vinklar bak med breda välmusklade lår. Rör sig med snabba fria rörelser. Utmärkt pälskvalitet. 
+          <br /><br />
+          Domare: HIERNER JULIA
+        </div>
        
         <div className="text-over-img">
           <h3>SDHK National Dog Show<br />Gimo september 2025<br />Best in Show 2 Unghund</h3>
